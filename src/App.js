85fad59e261d984d275f6efd8a1753feec348e4a
Albrim <albrim@Albrims-MacBook-Pro.local>
2024-02-28 16:42:51 +0100

    find job responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './LandingPage/Home/home';
-import Header from './LandingPage/components/header';
-import Login from './login/login';
-import FindJob from './pages/FindJob/findJob';
-import Signup from './signup/signup';
-import FAQs from './FAQs/faqs';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Trial from './pages/Trial/Trial';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import LoginMessage from './login/loginMessage';
-import NewHeader from './LandingPage/components/newHeader/newHeader';
-import ContactForm from './pages/ContactUs/Contact';
-import DetailsPage from './pages/DetailsPage/Details';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./LandingPage/Home/home";
+import Header from "./LandingPage/components/header";
+import Login from "./login/login";
+import FindJob from "./pages/FindJob/findJob";
+import Signup from "./signup/signup";
+import FAQs from "./FAQs/faqs";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Trial from "./pages/Trial/Trial";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoginMessage from "./login/loginMessage";
+import NewHeader from "./LandingPage/components/newHeader/newHeader";
+import ContactForm from "./pages/ContactUs/Contact";
+import DetailsPage from "./pages/DetailsPage/Details";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./LandingPage/Home/home";
+import Header from "./LandingPage/components/header";
+import Login from "./login/login";
+import FindJob from "./pages/FindJob/findJob";
+import Signup from "./signup/signup";
+import FAQs from "./FAQs/faqs";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Trial from "./pages/Trial/Trial";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoginMessage from "./login/loginMessage";
+import NewHeader from "./LandingPage/components/newHeader/newHeader";
+import ContactForm from "./pages/ContactUs/Contact";
 
 function App() {
   return (
@@ -27,14 +41,15 @@ function App() {
         )}
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/job" element={<FindJob />} /> */}
+          <Route path="/findjobs" element={<FindJob />} />
           <Route path="/register" element={<Login />} />
           {/* <Route path="/signup" element={<Signup />} /> */}
           <Route path="/faqs" element={<FAQs />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/welcome" element={<LoginMessage />} />
-          <Route path='/ContactUs' element={<ContactForm/>} />
-          <Route path='/DetailsPage' element={< DetailsPage/>}/>
+          <Route path="/ContactUs" element={<ContactForm />} />
+          <Route path="/DetailsPage" element={<DetailsPage />} />
+          <Route path="/ContactUs" element={<ContactForm />} />
         </Routes>
       </BrowserRouter>
     </>
