1fdc5da300b29026e19b393c1458b7f07f88704a
Urim <urimcanhasi1@gmail.com>
2024-03-19 20:15:38 +0100

    responsive find job

--- src/Freelancer/FindJob/findJob.js
+++ src/Freelancer/FindJob/findJob.js
@@ -8,11 +8,14 @@ import SortByFilter from "../../LandingPage/components/SortByFilter/sortByFilter
 import { IoSearchOutline } from "react-icons/io5";
 import { CiLocationOn } from "react-icons/ci";
 import { FaBookmark } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const FindJob = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [isTablet, setIsTable] = useState(false);
   const [jobs, setJobs] = useState([])
+  
+const navigate = useNavigate()
 
   const getJobs = () => {
     axios.get('').then(
@@ -43,10 +46,6 @@ const FindJob = () => {
     };
   }, []);
 
-  const handleClick = () => {
-    window.location.href = "/details-page";
-  };
-
   return (
     <div className="find-job">
       <div className="find-job-left">
@@ -69,64 +68,8 @@ const FindJob = () => {
           <div className="vert-barrier"></div>
           <div className="search-button">Search</div>
         </div>
+
         <div className="job-post-list">
-          <div className="job-post-container" onClick={handleClick}>
-            <div className="job-post-container-header">
-              <div className="jpch-left">
-                <img src={User2} alt="User" width={50} height={50} />
-                <div className="jpch-left-user">
-                  <h6 className="jpch-l-h6">Social Media</h6>
-                  <p className="jpch-l-p">By:Fjolla Berisha</p>
-                </div>
-              </div>
-              <div className="jpch-center">
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Location</p>
-                  <h6 className="jpch-c-value">Prishtina</h6>
-                </div>
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Type</p>
-                  <h6 className="jpch-c-value">Full-Time</h6>
-                </div>
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Category</p>
-                  <h6 className="jpch-c-value">Graphic Designer</h6>
-                </div>
-              </div>
-              <FaBookmark size={25} color="#455bef" />
-            </div>
-            <div className="job-post-container-body">
-              <p className="jpcb-p">
-                I'm on a mission to transform my room into a cozy sanctuary, and I'm reaching out to the creative minds out there for some inspiration!
-                <br />
-                <br />
-                We're seeking someone with a keen eye for candid moments and a knack for turning them into timeless memories. If you're.... Show more
-              </p>
-            </div>
-            <div className="footer-line"></div>
-            <div className="job-post-footer">
-              <div className="jp-footer-info">
-                <p className="tag">Kerkoj</p>
-                <p className="value">1 freelancer</p>
-              </div>
-              <div className="vert-barrier"></div>
-              <div className="jp-footer-info">
-                <div className="tag">Afati</div>
-                <div className="value">3 ditë</div>
-              </div>
-              <div className="vert-barrier"></div>
-              <div className="jp-footer-info">
-                <div className="tag">Budget</div>
-                <div className="value">4100$</div>
-              </div>
-              <button className="jp-apply-details">
-                <p className='a-d-p'>Apply</p>
-              </button>
-            </div>
-          </div>
           <div className="job-post-container">
             <div className="job-post-container-header">
               <div className="jpch-left">
@@ -179,69 +122,13 @@ const FindJob = () => {
                 <div className="tag">Budget</div>
                 <div className="value">4100$</div>
               </div>
-              <button className="jp-apply-details">
+              <button  onClick={()=>navigate('/details-page')} className="jp-apply-details">
                 <p className='a-d-p'>Apply</p>
               </button>
             </div>
           </div>
           <img src={Ads} alt="Banner" className="job-list-banner" />
-          <div className="job-post-container">
-            <div className="job-post-container-header">
-              <div className="jpch-left">
-                <img src={User2} alt="User" width={50} height={50} />
-                <div className="jpch-left-user">
-                  <h6 className="jpch-l-h6">Social Media</h6>
-                  <p className="jpch-l-p">By:Fjolla Berisha</p>
-                </div>
-              </div>
-              <div className="jpch-center">
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Location</p>
-                  <h6 className="jpch-c-value">Prishtina</h6>
-                </div>
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Type</p>
-                  <h6 className="jpch-c-value">Full-Time</h6>
-                </div>
-                <div className="vert-barrier"></div>
-                <div className="jpch-center-tags">
-                  <p className="jpch-c-tag">Category</p>
-                  <h6 className="jpch-c-value">Graphic Designer</h6>
-                </div>
-              </div>
-              <FaBookmark size={25} color="#455bef" />
-            </div>
-            <div className="job-post-container-body">
-              <p className="jpcb-p">
-                I'm on a mission to transform my room into a cozy sanctuary, and I'm reaching out to the creative minds out there for some inspiration!
-                <br />
-                <br />
-                We're seeking someone with a keen eye for candid moments and a knack for turning them into timeless memories. If you're.... Show more
-              </p>
-            </div>
-            <div className="footer-line"></div>
-            <div className="job-post-footer">
-              <div className="jp-footer-info">
-                <p className="tag">Kerkoj</p>
-                <p className="value">1 freelancer</p>
-              </div>
-              <div className="vert-barrier"></div>
-              <div className="jp-footer-info">
-                <div className="tag">Afati</div>
-                <div className="value">3 ditë</div>
-              </div>
-              <div className="vert-barrier"></div>
-              <div className="jp-footer-info">
-                <div className="tag">Budget</div>
-                <div className="value">4100$</div>
-              </div>
-              <button className="jp-apply-details">
-                <p className='a-d-p'>Apply</p>
-              </button>
-            </div>
-          </div>        </div>
+      </div>
       </div>
       <div className="find-job-right">
         <img src={AdBanner} alt="Advertising" className="find-job-right-banner" />
